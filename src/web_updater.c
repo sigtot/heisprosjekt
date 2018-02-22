46c6173da747b07f9a4d9625cd5cdb42bd264eb7
sigtot <sigurdtotland@gmail.com>
2018-02-22 17:51:08 +0100

    Get web orders and add them to the model order list

--- src/web_updater.c
+++ src/web_updater.c
@@ -3,6 +3,8 @@
 #include <sys/timeb.h>
 #include <curl/curl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
 
 void update_web_view() {
     int update_interval = 100;
@@ -34,24 +36,28 @@ void update_web_view() {
 }
 
 void get_orders_from_web() {
-    int update_interval = 100;
-    struct timeb timer_msec;
-    ftime(&timer_msec);
-    int milliseconds = timer_msec.millitm;
-    if(milliseconds % update_interval != 0) return;
+    /* Putting the curled data into a string turned out to be
+     * too much of a headache, so I'm just running curl
+     * directly :) This is bad, i know */
+    FILE *fp;
+    char path[1035];
 
-    CURL *curl;
-
-    /* Get a curl handle */
-    curl = curl_easy_init();
-    if(curl) {
-        char order_string [4];
-
-        curl_easy_setopt(curl, CURLOPT_URL, "159.65.58.116/heis-web/orders.php");
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &order_string);
-        curl_easy_perform(curl);
-
-        printf("%s", order_string);
-        curl_easy_cleanup(curl);
+    /* Open the command for reading */
+    fp = popen("/usr/bin/curl -s 159.65.58.116/heis-web/orders.php", "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n" );
+        exit(1);
     }
+
+    /* Read the output */
+    if (fgets(path, sizeof(path)-1, fp) != NULL && strlen(path) == 4) {
+        for(int i = 0; i < 4; i++) {
+            if(path[i] == '1') {
+                add_inside_order(i);
+            }
+        }
+    }
+
+    /* close */
+    pclose(fp);
 }
